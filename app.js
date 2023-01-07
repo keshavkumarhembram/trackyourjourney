477da4ec33644320b5c0a0c9b0f356491bf92235
keshavkumarhembram <keshavkumarhembram@gmail.com>
2023-01-07 11:37:47 +0530

    testing local server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./public"));
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
